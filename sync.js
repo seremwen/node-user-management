36d7b90e6cc873621b3937b9b2dca6bad940aa25
seremwen <seremwe.81@gmail.com>
2024-06-29 22:59:38 +0100

    updated sync file

--- sync.js
+++ sync.js
@@ -1,12 +1,6 @@
 // sync.js
 const sequelize = require('./models/database');
-const Member = require('./models/Member');
-const Certificate = require('./models/Certificate');
 
-
-// Set up associations
-// Member.hasOne(Certificate, { foreignKey: 'memberId', as: 'certificateDetails' });
-// Certificate.belongsTo(Member, { foreignKey: 'memberId', as: 'member' });
 
 sequelize.sync({ force: true }).then(() => {
     console.log('Database & tables created!');
